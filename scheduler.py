c97b9a6961291e522c6b278d4cda81530a2bf806
David Hand <davidhand@davidhand.com>
2022-09-25 22:49:56 -0400

    minor change to (non-)scheduler class

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,5 @@
 import asyncio
 import time
-from contextlib import suppress
-from argparse import ArgumentParser, BooleanOptionalAction
 
 # given a clock time, convert to monotonic loop time
 async def _time_time_to_loop_time(when):
@@ -16,10 +14,25 @@ async def run_in(delay, what):
     loop = asyncio.get_running_loop()
     return loop.call_later(delay, what)
 
-# given an interval in seconds, execute at the top of that interval
-# Especially: if the interval is a fraction of a second, execute
-#  at the beginning of the second, then at each equal fraction thereof.
+def _next_interval(t, interval):
+    r"""Given a time, find the beginning of the next whole interval.
+
+    :param t: a time in seconds, probably time.time()
+    :param b: an interval in seconds
+    :return: the time at which the next whole interval will begin
+    """
+    return ((t + interval) // interval) * interval
+
 async def run_every(interval, what):
+    r"""Given an interval in seconds, execute at the top of that interval.
+
+    Especially: if the interval is a fraction of a second, execute
+    at the beginning of the second, then at each equal fraction thereof.
+    """
+    # when does the next interval begin?
+    next_top = _next_interval(time.time(), interval)
+    # we probably ought to account for the possibility that the next
+    #   interval has already passed
     pass
 
 # lame example, but can't think of anything better
